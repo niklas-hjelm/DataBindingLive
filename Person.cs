c211c55f0aea53391fa1eda712bce49503bcd690
Niklas <niklas.hjelm@iths.se>
2022-10-06 13:59:43 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,18 @@
+﻿namespace DataBindingLive;
+
+public class Person
+{
+    private string _name;
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+
+    private int _age;
+    public int Age
+    {
+        get { return _age; }
+        set { _age = value; }
+    }
+}
